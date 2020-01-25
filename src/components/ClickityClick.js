cf166bdd5a8c0377fc9b3f68018e19ebc2cdb1c6
Sarah <saranderson13@gmail.com>
2020-01-25 18:23:14 -0500

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,37 @@
-// Code ClickityClick Component Here
+import React from 'react';
+
+class ClickityClick extends React.Component {
+
+    constructor() {
+        super();
+
+        // Set initial state.
+        this.state = {
+            hasBeenClicked: false
+        }
+    }
+
+    handleClick = () => {
+        // Update state.
+        this.setState({
+            hasBeenClicked: true
+        }, () => console.log(this.state.hasBeenClicked)) // prints true
+        console.log(this.state.hasBeenClicked) // prints false
+
+        // the above printouts are because setState() is asyncronous.
+        // it will run the next line of code, line 19, before it's finished
+        // running the setState call. 
+    }
+
+    render() {
+        return (
+            <div>
+                <p>I have {this.state.hasBeenClicked ? null : 'not'} been clicked!</p>
+                <button onClick={this.handleClick}>Click me!</button>
+            </div>
+
+        )
+    }
+}
+
+export default ClickityClick;
